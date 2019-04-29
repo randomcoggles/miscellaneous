a250bafec9ad8712f17e2d4c42e3b9fd17c4289f
randomcoggles <43040002+randomcoggles@users.noreply.github.com>
2019-04-29 14:32:13 -0300

    IsInteger function

---
+++ snippets.js
@@ -0,0 +1,35 @@
+function isInteger(n){       //
+      return (n - n) === 0;
+}
+
+var testArray = [
+    {value: null, shouldAvaluateToTrue: false},
+    {value: undefined, shouldAvaluateToTrue: false},
+    {value: '', shouldAvaluateToTrue: false},
+    {value: 'aw123sd', shouldAvaluateToTrue: false},
+    {value: ('str1' - 'str2'), shouldAvaluateToTrue: false},
+    {value: '123', shouldAvaluateToTrue: true},
+    {value: '12.3', shouldAvaluateToTrue: true},
+    {value: '123456789', shouldAvaluateToTrue: true},
+    {value: '123as', shouldAvaluateToTrue: true},
+    {value: '-123', shouldAvaluateToTrue: true}
+].forEach(item => {
+	const num = parseInt(item.value);
+	if(isInteger(num) === item.shouldAvaluateToTrue){
+		console.info( item.value + ' passed: ', num);
+    } else {
+		console.warn(item.value + ' passed: ', num);
+    }
+
+});
+
+// null passed:  NaN
+// undefined passed:  NaN
+//  passed:  NaN
+// aw123sd passed:  NaN
+// NaN passed:  NaN
+// 123 passed:  123
+// 12.3 passed:  12
+// 123456789 passed:  123456789
+// 123as passed:  123
+// -123 passed:  -123
